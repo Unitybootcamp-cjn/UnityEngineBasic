8e9ac162deb1002c7752f48242f05bfb0ca440e7
cjs100412 <cjs100412@gmail.com>
2025-04-07 09:49:07 +0900

    유니티 3match Bound 틀 맞추기 과제
    if문 switch문으로 변경

--- FirstProject/Assets/02.Scripts/InGame/LevelSystems/Bound.cs
+++ FirstProject/Assets/02.Scripts/InGame/LevelSystems/Bound.cs
@@ -7,58 +7,52 @@ namespace Match3.InGame.LevelSystems
     {
         // TODO : 블록의 갯수와 크기에 따라서 상하좌우로 Bound의 크기를 늘리고 줄인다.
         // 받아야 할 파라미터 : sizex, sizey(블록의 갯수), width, height(블록의 크기)
-
-        Transform _transform;
-        float _transformY;
-        float _transformX;
+        
         Map _map = new Map();
         Vector3 boundVector = new Vector3();
 
         private void Awake()
         {
             _map = FindAnyObjectByType<Map>(); // Map 클래스를 쓰는 오브젝트를 변수에 저장
-            _transform = GetComponent<Transform>(); // 이 컴포넌트의 Transform을 저장
         }
 
         private void Start()
         {
-            _transformX = (float)(_map.SizeX) * (_map.NodeWidth); // Map클래스의 SizeX(X축 블록의 갯수)와 NodeWidth(블록의 가로 길이)를 곱함.
-            _transformY = (float)(_map.SizeY) * (_map.NodeHeight);// Map클래스의 SizeY(Y축 블록의 갯수)와 NodeHeight(블록의 세로 길이)를 곱함.
+            float _transformX = (float)(_map.SizeX) * (_map.NodeWidth); // Map클래스의 SizeX(X축 블록의 갯수)와 NodeWidth(블록의 가로 길이)를 곱함.
+            float _transformY = (float)(_map.SizeY) * (_map.NodeHeight);// Map클래스의 SizeY(Y축 블록의 갯수)와 NodeHeight(블록의 세로 길이)를 곱함.
 
             // 이 컴포넌트의 이름을 확인하는 조건문
-            if (_transform.name == "Bound")
+            switch (transform.name)
             {
-                boundVector = new Vector3(-(_transformX / 2f + 0.5f - _map.BottomCenter.x),
+                case "Bound":
+                    boundVector = new Vector3(-(_transformX / 2f + 0.5f - _map.BottomCenter.x),
                                          ((_transformY / 2f) + _map.BottomCenter.y),
                                          _map.BottomCenter.z); // 해당 Bound에 맞는 Vector3좌표 계산
-                this.transform.localScale = new Vector3(1f, _transformY, 1f); // 해당 Bound에 맞는 스케일값 계산
-            }
-            else if(_transform.name == "Bound1")
-            {
-                boundVector = new Vector3((_transformX / 2f + 0.5f + _map.BottomCenter.x),
+                    transform.localScale = new Vector3(1f, _transformY, 1f); // 해당 Bound에 맞는 스케일값 계산
+                    break;
+                case "Bound1":
+                    boundVector = new Vector3((_transformX / 2f + 0.5f + _map.BottomCenter.x),
                                          ((_transformY / 2f) + _map.BottomCenter.y),
                                          _map.BottomCenter.z);
-                this.transform.localScale = new Vector3(1f, _transformY, 1f);
-            }
-            else if( _transform.name == "Bound2")
-            {
-                boundVector = new Vector3(_map.BottomCenter.x,
+                    transform.localScale = new Vector3(1f, _transformY, 1f);
+                    break;
+                case "Bound2":
+                    boundVector = new Vector3(_map.BottomCenter.x,
                                           _map.BottomCenter.y - 0.5f,
                                           _map.BottomCenter.z);
-                this.transform.localScale = new Vector3(_transformX + 2, 1f, 1f);
-            }
-            else if( _transform.name == "Bound3")
-            {
-                boundVector = new Vector3(_map.BottomCenter.x,
+                    transform.localScale = new Vector3(_transformX + 2, 1f, 1f);
+                    break;
+                case "Bound3":
+                    boundVector = new Vector3(_map.BottomCenter.x,
                                          ((_transformY / 2f) + _map.BottomCenter.y) - 0.5f,
                                          _map.BottomCenter.z + 1);
-                this.transform.localScale = new Vector3(_transformX + 2, _transformY + 1, 1f);
+                    transform.localScale = new Vector3(_transformX + 2, _transformY + 1, 1f);
+                    break;
+                default:
+                    Debug.LogError("이 컴포넌트는 Bound 컴포넌트가 아닙니다");
+                    break;
             }
-            else
-            {
-                Debug.LogError("이 컴포넌트는 Bound 컴포넌트가 아닙니다");
-            }
-            this.transform.position = boundVector; // 컴포넌트 위치 변경
+            transform.position = boundVector; // 컴포넌트 위치 변경
         }
     }
 }
