dec9bf27ef4b95f01b6aa837080f87f691e25e1a
cjs100412 <cjs100412@gmail.com>
2025-05-22 16:46:10 +0900

    Farm2D 프로젝트 생성 및 초기 구현
    플레이어 생성, 시네머신 카메라 설정, 타일맵을 이용한 맵 구현, 수확물 획득 구현

---
+++ Farm2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+
+[Serializable]
+public class PlayerStat
+{
+    public float speed; //플레이어의 이동 속도
+    public int count_of_harvest; //현재 수확물의 갯수
+}
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerStat stat;
+    Animator animator;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void SetAnimateMovement(Vector3 direction)
+    {
+        if(animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMove", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                {
+                    animator.SetBool("isMove", false);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector2(h, v);
+        SetAnimateMovement(dir);
+
+        transform.position += dir * stat.speed * Time.deltaTime;
+    }
+}
